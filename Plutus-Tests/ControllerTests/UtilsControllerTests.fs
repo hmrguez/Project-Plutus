88092e0f81f68cd7c4769f9d0cd74c4af9128319
vekt0R-HUB <zealot.algo@gmail.com>
2023-05-31 11:48:19 -0400

    Added push to docker hub

--- Plutus-Tests/ControllerTests/UtilsControllerTests.fs
+++ Plutus-Tests/ControllerTests/UtilsControllerTests.fs
@@ -12,7 +12,7 @@ type UtilsControllerTests() =
         let result = controller.GetGuid()
         let guid = result.Value :?> System.Guid
         
-        result.StatusCode.Should().Be(200, "because the GUID should be valid")
+        result.StatusCode.Should().Be(200, "because the GUID should be valid") |> ignore
         guid.ToString().Length.Should().Be(36, "Because the GUID should be valid")
         // System.Guid.TryParse(guid.ToString(), _) |> should equal true
         // System.Guid.TryParse(guid.ToString(), _) |> should equal true
